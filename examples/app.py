1f8a21e0987a8ae5feb7297ec2c223470bcc1fbb
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2017-01-31 08:21:27 +0100

    examples: sphinx documentation
    * Adds example app in Sphinx documentation.
    
    Signed-off-by: Lars Holm Nielsen <lars.holm.nielsen@cern.ch>

--- examples/app.py
+++ examples/app.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # This file is part of Invenio.
-# Copyright (C) 2015, 2016 CERN.
+# Copyright (C) 2015, 2016, 2017 CERN.
 #
 # Invenio is free software; you can redistribute it
 # and/or modify it under the terms of the GNU General Public License as
@@ -47,6 +47,9 @@ and open the example application in your browser:
 .. code-block:: console
 
     $ open http://127.0.0.1:5000/
+
+You should now be able to see that an error message have been logged to the
+console.
 
 To reset the example application run:
 
