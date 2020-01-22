ea84776ddd757dbeb843c68234e451e03f535603
Karolina Przerwa <karolina.m.przerwa@gmail.com>
2020-01-22 09:13:15 +0100

    global: bump minimal six version

--- docs/conf.py
+++ docs/conf.py
@@ -316,7 +316,7 @@ texinfo_documents = [
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
     'https://docs.python.org/': None,
-    'flask': ('http://flask.pocoo.org/docs/latest/', None),
+    'flask': ('https://flask.palletsprojects.com/en/master/', None),
 }
 
 # Autodoc configuraton.
