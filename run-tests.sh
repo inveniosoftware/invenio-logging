f880d30759ad3745a1fdda83ec8d069322a1d66d
Christoph Ladurner <christoph.ladurner@tugraz.at>
2022-06-14 23:01:50 +0200

    move check_manifest configuration to setup.cfg.
    concentrate the configuration of all calls in one place

--- run-tests.sh
+++ run-tests.sh
@@ -2,11 +2,12 @@
 #
 # This file is part of Invenio.
 # Copyright (C) 2015-2018 CERN.
+# Copyright (C) 2022 Graz University of Technology.
 #
 # Invenio is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
 pydocstyle invenio_logging && \
-check-manifest --ignore "*-requirements.txt" && \
+check-manifest && \
 sphinx-build -qnNW docs docs/_build/html && \
 python setup.py test
