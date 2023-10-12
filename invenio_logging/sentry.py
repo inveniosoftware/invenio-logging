db2e1aa40a923cc5e6a7f53b7ca68b1fd474320b
Zacharias Zacharodimos <zacharias.zacharodimos@cern.ch>
2023-10-12 16:27:15 +0200

    global: remove support for raven

--- invenio_logging/sentry.py
+++ invenio_logging/sentry.py
@@ -11,27 +11,11 @@
 from __future__ import absolute_import, print_function
 
 import logging
-import warnings
 
-import pkg_resources
-import six
 from flask import g
-from werkzeug.utils import import_string
 
 from . import config
 from .ext import InvenioLoggingBase
-
-try:
-    pkg_resources.get_distribution("raven")
-    from raven.processors import Processor
-except pkg_resources.DistributionNotFound:
-
-    class Processor(object):
-        """Dummy class in case Sentry is not installed.."""
-
-        def __init__(self, *args, **kwargs):
-            """Do nothing."""
-            pass
 
 
 class InvenioLoggingSentry(InvenioLoggingBase):
@@ -61,7 +45,6 @@ class InvenioLoggingSentry(InvenioLoggingBase):
         logging_exclusions = None
         if not app.config["LOGGING_SENTRY_PYWARNINGS"]:
             logging_exclusions = (
-                "raven",
                 "gunicorn",
                 "south",
                 "sentry.errors",
@@ -69,10 +52,8 @@ class InvenioLoggingSentry(InvenioLoggingBase):
                 "dill",
                 "py.warnings",
             )
-        if app.config["SENTRY_SDK"]:
-            self.install_sentry_sdk_handler(app, logging_exclusions, level)
-        else:
-            self.install_raven_handler(app, logging_exclusions, level)
+
+        self.install_sentry_sdk_handler(app, logging_exclusions, level)
 
         # Werkzeug only adds a stream handler if there's no other handlers
         # defined, so when Sentry adds a log handler no output is
@@ -113,42 +94,6 @@ class InvenioLoggingSentry(InvenioLoggingBase):
         with configure_scope() as scope:
             scope.level = level
 
-    def install_raven_handler(self, app, logging_exclusions, level):
-        """Install raven log handler."""
-        warnings.warn(
-            "The Raven library will be depricated.", PendingDeprecationWarning
-        )
-        from raven.contrib.celery import register_logger_signal, register_signal
-        from raven.contrib.flask import Sentry
-        from raven.handlers.logging import SentryHandler
-
-        cls = app.config["LOGGING_SENTRY_CLASS"]
-        if cls:
-            if isinstance(cls, six.string_types):
-                cls = import_string(cls)
-        else:
-            cls = Sentry
-        sentry = cls(
-            app,
-            logging=True,
-            level=level,
-            logging_exclusions=logging_exclusions,
-        )
-        app.logger.addHandler(SentryHandler(client=sentry.client, level=level))
-
-        # Capture warnings from warnings module
-        if app.config["LOGGING_SENTRY_PYWARNINGS"]:
-            self.capture_pywarnings(SentryHandler(sentry.client))
-
-        # Setup Celery logging to Sentry
-        if app.config["LOGGING_SENTRY_CELERY"]:
-            try:
-                register_logger_signal(sentry.client, loglevel=level)
-            except TypeError:
-                # Compatibility mode for Raven<=5.1.0
-                register_logger_signal(sentry.client)
-            register_signal(sentry.client)
-
     def add_request_id_sentry_python(self, event, hint):
         """Add the request id as a tag."""
         if g and hasattr(g, "request_id"):
@@ -156,16 +101,3 @@ class InvenioLoggingSentry(InvenioLoggingBase):
             tags.append(["request_id", g.request_id])
             event["tags"] = tags
         return event
-
-
-class RequestIdProcessor(Processor):
-    """Sentry event request processor for adding the request id as a tag."""
-
-    def process(self, data, **kwargs):
-        """Process event data."""
-        data = super(RequestIdProcessor, self).process(data, **kwargs)
-        if g and hasattr(g, "request_id"):
-            tags = data.get("tags", {})
-            tags["request_id"] = g.request_id
-            data["tags"] = tags
-        return data
