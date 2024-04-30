45f71374cd777ed8b491d0cfe640cebad67ba145
Alex Ioannidis <a.ioannidis@cern.ch>
2024-04-30 22:59:31 +0200

    📦 release: v2.1.1

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,12 +1,16 @@
 ..
     This file is part of Invenio.
-    Copyright (C) 2015-2019 CERN.
+    Copyright (C) 2015-2024 CERN.
 
     Invenio is free software; you can redistribute it and/or modify it
     under the terms of the MIT License; see LICENSE file for more details.
 
 Changes
 =======
+
+Version 2.1.1 (released 2024-04-30)
+
+- installation: upper pin Sentry-SDK
 
 Version 2.1.0 (released 2023-10-15)
 
--- invenio_logging/__init__.py
+++ invenio_logging/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # This file is part of Invenio.
-# Copyright (C) 2015-2018 CERN.
+# Copyright (C) 2015-2024 CERN.
 #
 # Invenio is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
@@ -87,6 +87,6 @@ For more information about logging please see:
 
 from __future__ import absolute_import, print_function
 
-__version__ = "2.1.0"
+__version__ = "2.1.1"
 
 __all__ = ("__version__",)
