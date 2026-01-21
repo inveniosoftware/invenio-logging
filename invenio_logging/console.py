ef9aa6c791aeee94031dd5ac121aa3888fe1367f
Sarah Wiechers <sarah.wiechers@uni-muenster.de>
2026-01-21 15:01:03 +0100

    logging: re-use flask default StreamHandler instead of adding new ones

--- invenio_logging/console.py
+++ invenio_logging/console.py
@@ -14,7 +14,7 @@ This extension is enabled by default and automatically installed via
 
 from __future__ import absolute_import, print_function
 
-import logging
+from flask.logging import default_handler
 
 from . import config
 from .ext import InvenioLoggingBase
@@ -45,13 +45,10 @@ class InvenioLoggingConsole(InvenioLoggingBase):
     def install_handler(self, app):
         """Install logging handler."""
         # Configure python logging
-        handler = logging.StreamHandler()
+        handler = default_handler
 
         if app.config["LOGGING_CONSOLE_PYWARNINGS"]:
             self.capture_pywarnings(handler)
 
         if app.config["LOGGING_CONSOLE_LEVEL"] is not None:
             handler.setLevel(app.config["LOGGING_CONSOLE_LEVEL"])
-
-        # Add the handler to the app logger
-        app.logger.addHandler(handler)
