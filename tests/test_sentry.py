7000e84ed4c33527075a3140cd45288af9477ec4
Mirek Simek <miroslav.simek@cesnet.cz>
2026-06-09 22:50:51 +0200

    fix: upgrade sentry-sdk from 1.* to 2.*
    * sentry-sdk api was changed from v1 to v2
      - last_event_id was at first removed, later
      added to sentry_sdk.api - see
      https://github.com/getsentry/sentry-python/issues/3049
    * added test to check that message is sent to sentry
      and last_event_id is generated
    * bumped versions of pytest-invenio and sentry-sdk

--- tests/test_sentry.py
+++ tests/test_sentry.py
@@ -2,6 +2,7 @@
 #
 # This file is part of Invenio.
 # Copyright (C) 2016-2018 CERN.
+# Copyright (C) 2026 CESNET z.s.p.o.
 #
 # Invenio is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
@@ -9,6 +10,9 @@
 """Sentry logging tests."""
 
 from __future__ import absolute_import, print_function
+
+import time
+from unittest.mock import patch
 
 from flask import Flask
 from sentry_sdk.hub import Hub
@@ -34,3 +38,29 @@ def test_init():
         assert Hub.current.get_integration(CeleryIntegration)
     else:
         assert Hub.current.get_integration(FlaskIntegration)
+
+
+def test_sentry_failure():
+    """Test that sentry works and logs a failure."""
+    import sentry_sdk
+
+    from invenio_logging.sentry import InvenioLoggingSentry
+
+    app = Flask("testapp")
+    app.config["SENTRY_DSN"] = "http://a-secret-hash@127.0.0.1:8000/1"
+    InvenioLoggingSentry(app)
+
+    sentry_transport = sentry_sdk.get_global_scope().client.transport
+
+    with patch.object(sentry_transport, "_send_request") as mock_send_request:
+        try:
+            1 / 0
+        except:
+            app.logger.exception("Division by zero")
+
+        # wait a bit for sentry-sdk to send the message
+        time.sleep(2)
+
+        mock_send_request.assert_called()
+
+    assert sentry_sdk.last_event_id() is not None
